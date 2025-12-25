02ec15796a1e712c1e7352d7aa6895f76d32b98a
AjaySurude <surudeajay9@gmail.com>
2025-12-25 16:45:49 +0530

    Add new hamberg menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("navMenu");
+// const menuToggle = document.getElementById("menuToggle");
+// const navMenu = document.getElementById("navMenu");
 
-menuToggle.addEventListener("click", () => {
-  navMenu.style.display =
-    navMenu.style.display === "flex" ? "none" : "flex";
-});
+// menuToggle.addEventListener("click", () => {
+//   navMenu.style.display =
+//     navMenu.style.display === "flex" ? "none" : "flex";
+// });
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#navMenu');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
