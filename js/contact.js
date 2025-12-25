7b70067cb2ca108eeca71694a44f3a1a8b5a2139
Sujit Khandave <khandavesujit75@gmail.com>
2025-12-26 01:26:36 +0530

    About and Contact Pages Added

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+
+    const items = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        }
+      });
+    }, {
+      threshold: 0.2
+    });
+
+    items.forEach(el => observer.observe(el));
